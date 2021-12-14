a067c07a146bc0bf52529698d9c37e5f743c34d0
bobby-216digital <bobby@216digital.com>
2021-12-13 21:27:07 -0500

    first commit

---
+++ components/Textbox.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Textbox() {
+    return (
+        <div className="border textbox">
+            <p>A textbox!</p>
+        </div>
+    )
+}
